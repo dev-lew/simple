c18e1984370990b347dd8a12ea9872f35d7b6acf
devlew <lewisd@bu.edu>
2023-08-12 14:03:37 -0400

    Add support for LessThan and Boolean

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,18 @@ trait Reducible {
 struct Number(i32);
 
 #[derive(Clone)]
+enum Boolean {
+    True,
+    False,
+}
+
+#[derive(Clone)]
+struct LessThan {
+    left: Box<Expression>,
+    right: Box<Expression>,
+}
+
+#[derive(Clone)]
 struct Add {
     left: Box<Expression>,
     right: Box<Expression>,
@@ -25,6 +37,8 @@ enum Expression {
     AddExpr(Add),
     MultiplyExpr(Multiply),
     NumberExpr(Number),
+    BooleanExpr(Boolean),
+    LessThanExpr(LessThan),
 }
 
 struct Machine {
@@ -49,8 +63,8 @@ impl Machine {
 impl Reducible for Expression {
     fn is_reducible(&self) -> bool {
         match self {
-            Expression::AddExpr(_) | Expression::MultiplyExpr(_) => true,
-            Expression::NumberExpr(_) => false,
+            Expression::AddExpr(_) | Expression::MultiplyExpr(_) | Expression::LessThanExpr(_)=> true,
+            Expression::NumberExpr(_) | Expression::BooleanExpr(_) => false,
         }
     }
 
@@ -58,7 +72,9 @@ impl Reducible for Expression {
         match self {
             Expression::AddExpr(x) => Add::reduce(x),
             Expression::MultiplyExpr(x) => Multiply::reduce(x),
+	    Expression::LessThanExpr(x) => LessThan::reduce(x),
             Expression::NumberExpr(x) => Expression::NumberExpr(x.clone()),
+	    Expression::BooleanExpr(x) => Expression::BooleanExpr(x.clone()),
             _ => panic!("Not Implemented"),
         }
     }
@@ -110,6 +126,29 @@ impl Multiply {
     }
 }
 
+impl LessThan {
+    fn reduce(&self) -> Expression {
+        if self.left.is_reducible() {
+            Expression::LessThanExpr(LessThan {
+                left: Box::new(self.left.reduce()),
+                right: self.right.clone(),
+            })
+        } else if self.right.is_reducible() {
+            Expression::LessThanExpr(LessThan {
+                left: self.left.clone(),
+                right: Box::new(self.right.reduce()),
+            })
+        } else {
+            match (&*self.left, &*self.right) {
+                (Expression::NumberExpr(Number(x)), Expression::NumberExpr(Number(y))) => {
+                    Expression::BooleanExpr(Boolean::from(x < y))
+                }
+                (_, _) => panic!("Invalid expression"),
+            }
+        }
+    }
+}
+
 impl fmt::Display for Number {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.0)
@@ -128,18 +167,45 @@ impl fmt::Display for Multiply {
     }
 }
 
+impl fmt::Display for Boolean {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	match self {
+	    Self::True => write!(f, "true"),
+	    Self::False => write!(f, "false"),
+	}
+    }
+}
+
+impl fmt::Display for LessThan {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} < {}", self.left, self.right)
+    }
+}
+
 impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Expression::AddExpr(x) => write!(f, "{}", x),
             Expression::MultiplyExpr(x) => write!(f, "{}", x),
             Expression::NumberExpr(x) => write!(f, "{}", x),
+	    Expression::LessThanExpr(x) => write!(f, "{}", x),
+	    Expression::BooleanExpr(x) => write!(f, "{}", x),
         }
     }
 }
 
+impl From<bool> for Boolean {
+    fn from(b: bool) -> Self {
+	if b {
+	    Self::True
+	} else {
+	    Self::False
+	}
+    }
+}
+
 fn main() {
-    let e = Expression::AddExpr(Add {
+    let a = Expression::AddExpr(Add {
         left: Box::new(Expression::NumberExpr(Number(5))),
         right: Box::new(Expression::MultiplyExpr(Multiply {
             left: Box::new(Expression::NumberExpr(Number(4))),
@@ -147,7 +213,7 @@ fn main() {
         })),
     });
 
-    let a = Expression::AddExpr(Add {
+    let b = Expression::AddExpr(Add {
         left: Box::new(Expression::NumberExpr(Number(5))),
         right: Box::new(Expression::NumberExpr(Number(4))),
     });
@@ -160,6 +226,7 @@ fn main() {
         })),
     });
 
-    let mut machine = Machine { expression: e };
+    let d = Expression::LessThanExpr(LessThan {left: Box::new(a), right: Box::new(c)});
+    let mut machine = Machine { expression: d };
     machine.run();
 }
